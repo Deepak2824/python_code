c3cb5e64383b8266c41e15dac325d18678611ac5
Deepak2824 <deepakkrdeepu6@gmail.com>
2023-10-18 00:11:40 +0530

    string

--- fourth_code.py
+++ fourth_code.py
@@ -4,3 +4,9 @@ print(my_string.lower())
 print(my_string.upper())
 new_string = "Hello Hello world"
 print(new_string.count("Hello"))
+
+s1 = "This is sparta!!!"
+print(s1.find('sparta'))
+
+s2 = "I like apples,mangoes,banana"
+print(s2.split(','))
