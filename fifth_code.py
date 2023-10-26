75d90f7846485959acedec30f829b6736c8455dc
Deepak2824 <deepakkrdeepu6@gmail.com>
2023-10-26 12:17:59 +0530

    tuple

--- fifth_code.py
+++ fifth_code.py
@@ -1,5 +1,14 @@
-tup1 = (1,"a",True,2,"b",False)
-print(tup1)
-print(tup1[0])
-print(tup1[-1])
-print(tup1[1:4])
+#tup1 = (1,"a",True,2,"b",False)
+#print(tup1)
+#print(tup1[0])
+#print(tup1[-1])
+#print(tup1[1:4])
+#find length of tuple
+#print(len(tup1))
+tup2 = (2,4,6)
+tup3 = (3,6,9)
+print(tup2+tup3)
+print(tup2*3) #Reapeating tuple element
+print(min(tup2))
+print(max(tup2))
+
